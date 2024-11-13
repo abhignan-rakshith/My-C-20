61b23f594e398203f0ff2da55f04c3cb9eb78b34
Abhignan_R <abhignanrakshith@outlook.com>
2024-11-13 17:05:32 +0530

    First Steps - First_Prgm, Comments, Errors

---
+++ 01 - First Steps/Comments/main.cpp
@@ -0,0 +1,17 @@
+
+// This brings in the IO stream library
+#include <iostream>
+
+/**
+ * This is a block comment
+ * It is a multi-line doc 
+*/
+
+
+int main(){
+    // This is going to print "Hello World" to the console
+    std::cout << "Hello World" << std::endl;
+
+    // Program ends here
+    return 0;
+}
---
+++ 01 - First Steps/First_Cpp_Program/main.cpp
@@ -0,0 +1,14 @@
+// Feature to load the pre-buit library (console print and read)
+#include <iostream>
+
+// main() funtion, starting point of c++ program
+int main(){
+    std::cout << "Hello World" << std::endl; // endl prints a new line
+
+    std::cout << "Number 1" << std::endl;
+    std::cout << "Number 2 ";
+    std::cout << "Number 3";
+
+// This is to let the OS know, if out program ran succesfully ran or no.
+    return 0;
+}

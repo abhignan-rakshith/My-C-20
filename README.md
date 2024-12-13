7e0e21dd61aebebd9fb05b3da440a281145710ab
Abhignan Rakshith <abhignanrakshith@outlook.com>
2024-12-13 21:46:41 +0530

    First Commit from homw with complete setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The Start of my beautiful C++ journey.
+Now started working from home also, this is from Home_Pc!
---
+++ Template_Win_Home/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Let's Win in Life!" << std::endl;
+    return 0;
+}
